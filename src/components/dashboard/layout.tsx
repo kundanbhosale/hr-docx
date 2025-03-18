796c4dfa16938910eb77b1b023bfd4e8ccd96dfe
Kundan Bhosale <kundanmbhosale@gmail.com>
2025-03-18 22:14:00 +0530

    updates

--- src/components/dashboard/layout.tsx
+++ src/components/dashboard/layout.tsx
@@ -67,8 +67,6 @@ export const DashboardLayout = ({
                 className={cn(
                   "flex items-center justify-center text-lg font-semibold py-2 border-b border-border/20 truncate rounded-md",
                   open && "justify-start rounded-none",
-                  !open && "hover:bg-muted",
-
                   active === n.url && "bg-primary"
                 )}
                 href={n.url}
@@ -99,7 +97,6 @@ export const DashboardLayout = ({
               className={cn(
                 "flex items-center justify-center text-lg font-semibold py-2 border-b border-border/20 truncate rounded-md",
                 open && "justify-start rounded-none",
-                !open && "hover:bg-muted",
                 active === n.url && "bg-primary"
               )}
               href={n.url}
